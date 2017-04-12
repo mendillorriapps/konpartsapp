7813991fd9f463ab1f8730be626a2801140ebd84
Mendillorri Apps <mendillorriapps@educacion.navarra.es>
2017-04-12 10:18:04 +0200

    Bikoteak hobetuta

--- bikoteak00.py
+++ bikoteak00.py
@@ -15,35 +15,42 @@ class MyApp(App):
 
         dena = GridLayout(cols=4,spacing=20)
 
-        A = "/home/euskera/Mahaigaina/Deskargak/erraldoia_00.png"
-        B = "/home/euskera/Mahaigaina/Deskargak/erraldoia_01.png"
-        C = "/home/euskera/Mahaigaina/Deskargak/erraldoia_02.png"
-        D = "/home/euskera/Mahaigaina/Deskargak/erraldoia_03.png"
-        C = "/home/euskera/Mahaigaina/Deskargak/erraldoia_05.png"
-        D = "/home/euskera/Mahaigaina/Deskargak/erraldoia_06.png"
-        E = "/home/euskera/Mahaigaina/Deskargak/erraldoia_07.png"
-        F = "/home/euskera/Mahaigaina/Deskargak/erraldoia_08.png"
+        A = "/home/euskera/Mahaigaina/Dokumentuak/Irantzu/erraldoia_00.png"
+        B = "/home/euskera/Mahaigaina/Dokumentuak/Irantzu/erraldoia_01.png"
+        C = "/home/euskera/Mahaigaina/Dokumentuak/Irantzu/erraldoia_02.png"
+        D = "/home/euskera/Mahaigaina/Dokumentuak/Irantzu/erraldoia_03.png"
+        C = "/home/euskera/Mahaigaina/Dokumentuak/Irantzu/erraldoia_05.png"
+        D = "/home/euskera/Mahaigaina/Dokumentuak/Irantzu/erraldoia_06.png"
+        E = "/home/euskera/Mahaigaina/Dokumentuak/Irantzu/erraldoia_07.png"
+        F = "/home/euskera/Mahaigaina/Dokumentuak/Irantzu/erraldoia_08.png"
 
         self.answer = list([A,A,B,B,C,C,D,D,E,E,F,F])
         random.shuffle(self.answer)
         self.answer = [self.answer[:4],
                        self.answer[4:8],
                        self.answer[8:12],
-                       self.answer[12:]]
+                       self.answer[12:16]]
+        print(self.answer)
         for row in self.answer:
             for irudia in row:
-               btn1 = Button(text="harria")
+               btn1 = Button()
+               btn1.background_normal = "/home/euskera/Mahaigaina/Dokumentuak/Irantzu/buelta.jpg"
+               btn1.erraldoia =  irudia
                btn1.bind(on_press=self.jokatu)
                dena.add_widget(btn1)
         return dena
 
     def jokatu(self, botoia):
-        aukera = botoia.text
-        botoia.text = "kaixo"
+        if botoia.background_normal == botoia.erraldoia:
+            botoia.background_normal = "/home/euskera/Mahaigaina/Dokumentuak/Irantzu/buelta.jpg"
+        else:
+            botoia.background_normal = botoia.erraldoia
         popup = Popup(title="Irabazlea",
             content=Label(text= ", ordenagailuak  aukeratu duelako "),
             size_hint=(None, None), size=(400, 400))
-        popup.open()
+        #popup.open()
+
+
 
 
 if __name__ == '__main__':
