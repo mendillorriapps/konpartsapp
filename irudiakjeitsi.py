1e9c916e27886adc21e31b18f003e1a60d743a7d
Mendillorri Apps <mendillorriapps@educacion.navarra.es>
2017-04-24 05:45:52 +0200

    irudiak egiteko programa bukatuta

--- irudiakjeitsi.py
+++ irudiakjeitsi.py
@@ -1,15 +1,17 @@
 import csv
 import requests
+izenak=["Pamplona","mendillorri"]
+for izena in izenak:
+    data={}
+    with open('/home/euskera/Mahaigaina/Dokumentuak/JULEN/konpartsapp/parametroak.csv') as fin:
+        reader=csv.reader(fin, skipinitialspace=True, quotechar="'")
+        for row in reader:
+            data[row[0]]=row[1]
 
-data={}
-with open('/home/euskera/Mahaigaina/Dokumentuak/JULEN/konpartsapp/parametroak.csv') as fin:
-    reader=csv.reader(fin, skipinitialspace=True, quotechar="'")
-    for row in reader:
-        data[row[0]]=row[1]
-
-r = requests.get('https://dabuttonfactory.com/button.png', params=data)
-print(r.url,r.status_code)
-with open("irudia.png", 'wb') as fd:
-    for chunk in r.iter_content(chunk_size=128):
-        fd.write(chunk)
-print(data)
+    data["t"]=izena
+    r = requests.get('https://dabuttonfactory.com/button.png', params=data)
+    print(r.url,r.status_code)
+    with open(izena+".png", 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=128):
+            fd.write(chunk)
+    print(data)
